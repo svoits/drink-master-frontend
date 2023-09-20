3571ed57d312873ba528894c2e43d38e1f7fc87b
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-09-20 17:19:48 +0200

    add initial structure

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_vite">
+    <BrowserRouter basename="/drink-master-frontend">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
