94c226cb900444c12a7e173cb9db9840fbbc690e
NGanch <86801593+NGanch@users.noreply.github.com>
2023-09-22 16:25:23 +0100

    comit

---
+++ src/components/DrinksListPage/DrinksListPage.styled.jsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const DrinksPageLIST = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+    gap: 14px;
+`;
+
+export const DrinksPageITEM = styled.li`
+    width: 400px;
+    height: 438px;
+`;
+
+export const DrinksPageIMG = styled.img`
+width: 400px;
+height: 400px;
+border-radius: 8px;
+`;
+
+export const DrinksPageTEXTDIV = styled.div`
+display: flex;
+gap: 148px;
+align-items: center;
+
+`;
+export const DrinksPageP = styled.p`
+font-family: Manrope;
+color: #F3F3F3;
+font-size: 18px;
+font-weight: 500;
+line-height: 24px;
+letter-spacing: 0em;
+text-align: left;
+`;
+export const DrinksPageA = styled.p`
+font-family: Manrope;
+font-size: 16px;
+font-weight: 500;
+line-height: 18px;
+letter-spacing: 0em;
+text-align: right;
+color: #F3F3F380;
+
+`;
---
+++ src/pages/DrinksPage/DrinksPage.styled.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const DrinksPageTITLE = styled.h1`
+margin-top: 160px;
+font-family: Manrope;
+font-size: 64px;
+font-weight: 600;
+line-height: 68px;
+letter-spacing: 0em;
+text-align: left;
+color: #F3F3F3;
+`;
+
+
+
