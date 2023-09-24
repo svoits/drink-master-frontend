02f99d0e5b3d0f6bd3c639a4eeb8f40ddaebdbf1
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-09-24 18:25:27 +0200

    Update filters-operation.js

--- src/redux/filters/filters-operation.js
+++ src/redux/filters/filters-operation.js
@@ -1,39 +1,39 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-axios.defaults.baseURL = 'https://drink-master-api.onrender.com';
+// axios.defaults.baseURL = 'https://drink-master-api.onrender.com';
 
 export const getCategories = createAsyncThunk(
-    'filters/getCategories',
-    async (data, thunkAPI) => {
-        try {
-            const response = await axios.get('/categories', data);
-            return response.data;
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-    },
+  'filters/getCategories',
+  async (data, thunkAPI) => {
+    try {
+      const response = await axios.get('/categories', data);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
 );
 
 export const getIngridients = createAsyncThunk(
-    'filters/getIngridients',
-    async (data, thunkAPI) => {
-        try {
-            const response = await axios.get('/ingridients', data);
-            return response.data;
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-    },
+  'filters/getIngridients',
+  async (data, thunkAPI) => {
+    try {
+      const response = await axios.get('/ingridients', data);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
 );
 
 export const getGlasses = createAsyncThunk(
-    'filters/getGlasses',
-    async (data, thunkAPI) => {
-        try {
-            const response = await axios.get('/glasses', data);
-            return response.data;
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-    },
+  'filters/getGlasses',
+  async (data, thunkAPI) => {
+    try {
+      const response = await axios.get('/glasses', data);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
 );
