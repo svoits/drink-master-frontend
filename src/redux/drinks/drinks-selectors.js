5d83157d010e5761d65aa3b33e0d60deba143b81
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-09-25 19:11:08 +0200

    Update drinks-selectors.js

--- src/redux/drinks/drinks-selectors.js
+++ src/redux/drinks/drinks-selectors.js
@@ -1,5 +1,6 @@
 export const selectIsLoading = (state) => state.isLoading;
 export const selectDrinks = (state) => state.drinks;
+export const selectErrorDrinks = (state) => state.drinks.error;
 export const selectDrinkById = (state, drinkId) =>
   state.drinks.drinks.find((drink) => drink._id === drinkId);
 
