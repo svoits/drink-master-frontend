38179e6dcd9c052f3d4690422ad233091601b2a6
Illia Shulha <ilyxa2836@gmail.com>
2023-09-26 11:05:07 +0200

    fixes fonts

--- src/components/globalStyles.js
+++ src/components/globalStyles.js
@@ -2,42 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
 
-@font-face {
-  font-family: 'Manrope';
-  font-weight: 400;
-  font-style: normal;
-  font-display: swap;
-  src: local('Manrope Regular'), local('Manrope-Regular'),
-    url('public/fonts/Manrope-Regular.woff2') format('woff2'),
-    url('public/fonts/Manrope-Regular.woff') format('woff'),
-    url('public/fonts/Manrope-Regular.ttf') format('truetype'),
-    url('public/fonts/Manrope-Regular.eot') format('embedded-opentype');
-}
-
-@font-face {
-  font-family: 'Manrope';
-  font-weight: 500;
-  font-style: normal;
-  font-display: swap;
-  src: local('Manrope Medium'), local('Manrope-Medium'),
-    url('public/fonts/Manrope-Medium.woff2') format('woff2'),
-    url('public/fonts/Manrope-Medium.woff') format('woff'),
-    url('public/fonts/Manrope-Medium.ttf') format('truetype'),
-    url('public/fonts/Manrope-Medium.eot') format('embedded-opentype'),
-
-}
-
-@font-face {
-  font-family: 'Manrope';
-  font-weight: 600;
-  font-style: normal;
-  font-display: swap;
-  src: local('Manrope SemiBold'), local('Manrope-SemiBold'),
-    url('public/fonts/Manrope-SemiBold.woff2') format('woff2'),
-    url('public/fonts/Manrope-SemiBold.woff') format('woff'),
-    url('public/fonts/Manrope-SemiBold.ttf') format('truetype'),
-    url('public/fonts/Manrope-SemiBold.eot') format('embedded-opentype');
-}
 html, body, #root {
   height: 100%;
 }
