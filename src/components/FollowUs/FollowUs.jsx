cb3ce59e18a51a74d97c765685a8fb0ee5a63e86
AllaKaplia <kaplia.alla18@gmail.com>
2023-09-28 00:20:26 +0300

    Update FollowUs.jsx

--- src/components/FollowUs/FollowUs.jsx
+++ src/components/FollowUs/FollowUs.jsx
@@ -1,28 +1,10 @@
-import { TfiFacebook } from 'react-icons/tfi';
-import { BiLogoInstagramAlt } from 'react-icons/bi';
-import { AiFillYoutube } from 'react-icons/ai';
+import SocialIcons from '../SocialIcons/SocialIcons';
 
 const FollowUs = () => {
     return (
         <div>
             <h3>Follow Us</h3>
-            <ul>
-                <li>
-                    <a href="https://www.facebook.com/goITclub/" target="_blank" rel="noreferrer">
-                        <TfiFacebook />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.instagram.com/goitclub/" target="_blank" rel="noreferrer">
-                        <BiLogoInstagramAlt />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.youtube.com/c/GoIT" target="_blank" rel="noreferrer">
-                        <AiFillYoutube />
-                    </a>
-                </li>
-            </ul>
+            <SocialIcons />
         </div>
     )
 };
