91cc7604fe46c3be0d231b58af2203ce4e414be4
AllaKaplia <kaplia.alla18@gmail.com>
2023-09-27 17:44:30 +0300

    Create AddDrinkForm.jsx

---
+++ src/components/AddDrinkForm/AddDrinkForm.jsx
@@ -0,0 +1,14 @@
+import DrinkDescriptionFields from "../DrinkDescriptionFields/DrinkDescriptionFields";
+
+
+const AddDrinkForm = () => {
+
+    return (
+        <>
+            <DrinkDescriptionFields />
+        </>
+
+    )
+};
+
+export default AddDrinkForm;
