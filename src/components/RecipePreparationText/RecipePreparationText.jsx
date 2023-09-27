58583bdceab5bb8d61bdc2ff5748364f9fcbf51d
AllaKaplia <kaplia.alla18@gmail.com>
2023-09-28 00:20:31 +0300

    Update RecipePreparationText.jsx

--- src/components/RecipePreparationText/RecipePreparationText.jsx
+++ src/components/RecipePreparationText/RecipePreparationText.jsx
@@ -1,10 +1,19 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
+const validationSchema = Yup.object().shape({
+    textareaRecipe: Yup.string()
+      .min(100, 'You need to add a description of at least 100 symbols')
+});
+
+const initialValue = {
+    textareaRecipe: '',
+}
 
 const RecipePreparationText = () => {
     return(
-        <div>
-            <Formik> 
+        <>
+            <Formik initialValues={initialValue} validationSchema={validationSchema}> 
                 <Form>
                     <h2 htmlFor="textareaRecipe">Recipe Preparation</h2>
                     <Field name="textareaRecipe">
@@ -15,7 +24,7 @@ const RecipePreparationText = () => {
                     <ErrorMessage name="textareaValue" component="div" />
                 </Form>
             </Formik>
-        </div>
+        </>
     )
 };
 
