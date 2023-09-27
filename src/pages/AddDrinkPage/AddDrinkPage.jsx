a1186aacaa314d10875e1e3ed8d6c50d2994a475
AllaKaplia <kaplia.alla18@gmail.com>
2023-09-28 02:21:01 +0300

    Update AddDrinkPage.jsx

--- src/pages/AddDrinkPage/AddDrinkPage.jsx
+++ src/pages/AddDrinkPage/AddDrinkPage.jsx
@@ -1,12 +1,12 @@
 import AddDrinkForm from '../../components/AddDrinkForm/AddDrinkForm';
 import FollowUs from '../../components/FollowUs/FollowUs';
 import PopularDrinks from '../../components/PopularDrinks/PopularDrinks';
-// import { PageTitle } from '../../components/PageTitle/PageTitle';
+import PageTitle from '../../components/PageTitle/PageTitle';
 
 export default function AddDrinkPage() {
   return (
     <div>
-      {/* <PageTitle /> */}
+      <PageTitle title="Add drink"/>
       <AddDrinkForm />
       <FollowUs />
       <PopularDrinks />
