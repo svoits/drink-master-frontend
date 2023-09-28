32bf331d5f4433dbfd38f77d850796a4cc519e26
AllaKaplia <kaplia.alla18@gmail.com>
2023-09-28 22:05:10 +0300

    Update DrinkDescriptionFields.styled.jsx

--- src/components/DrinkDescriptionFields/DrinkDescriptionFields.styled.jsx
+++ src/components/DrinkDescriptionFields/DrinkDescriptionFields.styled.jsx
@@ -1,3 +1,4 @@
+import { Field } from 'formik';
 import styled from 'styled-components';
 // import transition from '../../utils/transition';
 
@@ -5,4 +6,20 @@ export const PhotoContainer = styled.label`
     display: block;
     width: 335px;
     height: 320px;
+`;
+
+export const PhotoPreview = styled.img`
+    max-width: 100%;
+    max-height: 100%;
+    width: auto;
+    height: auto;
+`;
+
+export const PhotoField = styled(Field)`
+    position: absolute;
+    width: 0;
+    height: 0;
+    opacity: 0;
+    visibility: hidden;
+    overflow: hidden;
 `;
