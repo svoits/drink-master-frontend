5b085348120e89be61a84ee745ae768fed351bdc
AllaKaplia <kaplia.alla18@gmail.com>
2023-09-28 19:20:02 +0300

    Update DrinkDescriptionFields.styled.jsx

---
+++ src/components/DrinkDescriptionFields/DrinkDescriptionFields.styled.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+// import transition from '../../utils/transition';
+
+export const PhotoContainer = styled.label`
+    display: block;
+    width: 335px;
+    height: 320px;
+`;
