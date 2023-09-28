b2810652ece0e9e8f08b44cba9e34063756644a8
nataprofits <nataprofits@gmail.com>
2023-09-28 12:53:53 +0100

    changed Defolt Favorite Page

--- src/pages/FavoriteDrinksPage/FavoriteDrinksPage.jsx
+++ src/pages/FavoriteDrinksPage/FavoriteDrinksPage.jsx
@@ -4,9 +4,15 @@ import PageTitle from '../../components/PageTitle/PageTitle';
 import { getFavoriteAll } from '../../redux/drinks/drinks-operations';
 import { selectFavoriteDrinks } from '../../redux/drinks/drinks-selectors';
 import FavoriteDrinkList from '../../components/FavoriteDrinkList/FavoriteDrinkList';
-import Loader from '../../components/Loader';
+import {
+  DefaultContainer,
+  DefaultDescr,
+  DefaultImg,
+  FavoritesContainer,
+} from './FavoriteDrinksPage.styled';
 import { Container } from '../../components/Container/Container.styled';
-
+import CoctailImage from '../../images/heroImage/hero-img-desc-2x.png';
+import Loader from '../../components/Loader';
 export default function FavoriteDrinksPage() {
   const dispatch = useDispatch();
   const favoriteDrinks = useSelector(selectFavoriteDrinks);
@@ -21,16 +27,22 @@ export default function FavoriteDrinksPage() {
 
   return (
     <Container>
-      <PageTitle title="Favorite" />
-      {isLoading && <Loader />}
-      {errorMessage && <div>{errorMessage}</div>}
-      <div>
-        {favoriteDrinks.length !== 0 ? (
-          <FavoriteDrinkList drinks={favoriteDrinks} />
-        ) : (
-          <p>You have not added any cocktails yet</p>
+      <FavoritesContainer>
+        <PageTitle title="Favorite" />
+        {isLoading && <Loader />}
+        {favoriteDrinks.length === 0 && (
+          <DefaultContainer>
+            <DefaultImg src={CoctailImage} alt="Cocktail" />
+            <DefaultDescr>You have not added any cocktails yet</DefaultDescr>
+          </DefaultContainer>
         )}
-      </div>
+        {favoriteDrinks.length > 0 && (
+          <div>
+            <FavoriteDrinkList drinks={favoriteDrinks} />
+          </div>
+        )}
+        {errorMessage && <div>{errorMessage}</div>}
+      </FavoritesContainer>
     </Container>
   );
 }
--- src/pages/FavoriteDrinksPage/FavoriteDrinksPage.styled.jsx
+++ src/pages/FavoriteDrinksPage/FavoriteDrinksPage.styled.jsx
@@ -1,5 +1,22 @@
 import styled from 'styled-components';
 
+export const FavoritesContainer = styled.div`
+  width: 100%;
+  margin: 0;
+  padding-top: 80px;
+  padding-bottom: 80px;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 140px;
+    padding-bottom: 140px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding-top: 158px;
+    padding-bottom: 140px;
+  }
+`;
+
 export const DrinksFavoriveTITLE = styled.h1`
   margin-top: 160px;
   margin-bottom: 62px;
@@ -10,4 +27,59 @@ export const DrinksFavoriveTITLE = styled.h1`
   letter-spacing: 0em;
   text-align: left;
   color: ${({ theme }) => theme.mainText};
+`;
+
+export const StyledDefaultContainer = styled.div`
+  margin: 0 auto;
+  padding-top: 91px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 100px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding-top: 67px;
+  }
+`;
+export const DefaultContainer = styled.div`
+  margin: 0 auto;
+  padding-top: 91px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 100px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding-top: 67px;
+  }
+`;
+export const DefaultDescr = styled.p`
+  width: 205px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.28;
+  margin-top: 32px;
+  text-align: center;
+
+  @media screen and (min-width: 768px) {
+    width: 236px;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 1.38;
+  }
+`;
+export const DefaultImg = styled.img`
+  width: 198px;
+  height: 247px;
+
+  @media screen and (min-width: 768px) {
+    width: 261px;
+    height: 326px;
+  }
 `;
