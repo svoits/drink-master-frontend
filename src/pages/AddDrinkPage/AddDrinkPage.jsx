0aac38f53bec4115025c0776b1762140c4b6e7b3
AllaKaplia <kaplia.alla18@gmail.com>
2023-09-28 20:49:43 +0300

    Update AddDrinkPage.jsx

--- src/pages/AddDrinkPage/AddDrinkPage.jsx
+++ src/pages/AddDrinkPage/AddDrinkPage.jsx
@@ -3,7 +3,9 @@ import FollowUs from '../../components/FollowUs/FollowUs';
 import PopularDrinks from '../../components/PopularDrinks/PopularDrinks';
 import PageTitle from '../../components/PageTitle/PageTitle';
 
+
 export default function AddDrinkPage() {
+
   return (
     <div>
       <PageTitle title="Add drink"/>
