e7fcba854e1818c95116322f78cc03057ce5b5a2
nikolashchuk <nikolashchuk@gmail.com>
2023-09-29 20:01:55 +0300

    add-base-style-for-PopularDrinks

--- src/components/PopularDrinks/PopularDrinks.jsx
+++ src/components/PopularDrinks/PopularDrinks.jsx
@@ -1,37 +1,47 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getPopularDrinks } from "../../redux/drinks/drinks-operations";
-import { selectDrinks } from "../../redux/drinks/drinks-selectors";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPopularDrinks } from '../../redux/drinks/drinks-operations';
+import { selectDrinks } from '../../redux/drinks/drinks-selectors';
 
 import Loader from '../Loader/Loader';
-
+import {
+  DrinksImg,
+  DrinksInfoDescr,
+  DrinksInfoTitle,
+  DrinksItem,
+  DrinksLink,
+  DrinksList,
+  InfoWrap,
+} from './PopularDrinks.styled';
 
 const PopularDrinks = () => {
-    const dispatch = useDispatch();
-    const popularList = useSelector(state => state.drinks.popularDrinks);
-    const { isLoading, error } = useSelector(selectDrinks);
+  const dispatch = useDispatch();
+  const popularList = useSelector((state) => state.drinks.popularDrinks);
+  const { isLoading, error } = useSelector(selectDrinks);
 
-    useEffect(() => {
-        dispatch(getPopularDrinks());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getPopularDrinks());
+  }, [dispatch]);
 
-    return (
-        <div>
-            {isLoading && <Loader />}
-            {error && <p>Sorry. There are no images ... 😭</p>}
-            <ul>
-                {popularList.map(({ _id, drinkThumb, drink, shortDescription }) => (
-                    <li key={_id}>
-                        <a href={`/api/drinks/popular/${_id}`}>
-                            <img src={drinkThumb} alt={drink} width={90} height={90} />
-                            <h3>{drink}</h3>
-                            <p>{shortDescription}</p>
-                        </a>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  return (
+    <div>
+      {isLoading && <Loader />}
+      {error && <p>Sorry. There are no images ... 😭</p>}
+      <DrinksList>
+        {popularList.map(({ _id, drinkThumb, drink, shortDescription }) => (
+          <DrinksItem key={_id}>
+            <DrinksLink href={`/api/drinks/popular/${_id}`}>
+              <DrinksImg src={drinkThumb} alt={drink} width={90} height={90} />
+              <InfoWrap>
+                <DrinksInfoTitle>{drink}</DrinksInfoTitle>
+                <DrinksInfoDescr>{shortDescription}</DrinksInfoDescr>
+              </InfoWrap>
+            </DrinksLink>
+          </DrinksItem>
+        ))}
+      </DrinksList>
+    </div>
+  );
 };
 
 export default PopularDrinks;
