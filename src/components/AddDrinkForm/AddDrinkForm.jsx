49af65a6b5784ee5fcdf18a2beb76116b3f3bd26
nikolashchuk <nikolashchuk@gmail.com>
2023-09-29 23:32:53 +0300

    add-style-addDrink

--- src/components/AddDrinkForm/AddDrinkForm.jsx
+++ src/components/AddDrinkForm/AddDrinkForm.jsx
@@ -1,52 +1,46 @@
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { addMyDrink } from '../../redux/drinks/drinks-operations';
-import DrinkDescriptionFields from "../DrinkDescriptionFields/DrinkDescriptionFields";
-import DrinkIngredientsFields from "../DrinkIngredientsFields";
-import RecipePreparationText from "../RecipePreparationText/RecipePreparationText";
+import DrinkDescriptionFields from '../DrinkDescriptionFields/DrinkDescriptionFields';
+import DrinkIngredientsFields from '../DrinkIngredientsFields';
+import RecipePreparationText from '../RecipePreparationText/RecipePreparationText';
+import { AddDrinkFormBtn } from './AddDrinkForm.styled';
 
 const AddDrinkForm = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const [formData, setFormData] = useState({
-        photo: '',
-        title: '',
-        recipe: '',
-        category: '',
-        glasses: '',
-        strength: '',
-        ingredients: [
-            { ingredient: '', measure: '', quantity: '' },
-            { ingredient: '', measure: '', quantity: '' },
-            { ingredient: '', measure: '', quantity: '' },
-        ],
-        textareaRecipe: '',
-    });
+  const [formData, setFormData] = useState({
+    photo: '',
+    title: '',
+    recipe: '',
+    category: '',
+    glasses: '',
+    strength: '',
+    ingredients: [
+      { ingredient: '', measure: '', quantity: '' },
+      { ingredient: '', measure: '', quantity: '' },
+      { ingredient: '', measure: '', quantity: '' },
+    ],
+    textareaRecipe: '',
+  });
 
-    const handleSubmit = () => {
-        dispatch(addMyDrink(formData));
-        console.log(formData);
-    };
+  const handleSubmit = () => {
+    dispatch(addMyDrink(formData));
+    console.log(formData);
+  };
 
-    return (
-        <>
-            <DrinkDescriptionFields
-                formData={formData}
-                setFormData={setFormData}
-            />
+  return (
+    <>
+      <DrinkDescriptionFields formData={formData} setFormData={setFormData} />
 
-            <DrinkIngredientsFields
-                formData={formData}
-                setFormData={setFormData}
-            />
-            <RecipePreparationText
-                formData={formData}
-                setFormData={setFormData}
-            />
+      <DrinkIngredientsFields formData={formData} setFormData={setFormData} />
+      <RecipePreparationText formData={formData} setFormData={setFormData} />
 
-            <button type="button" onClick={handleSubmit}>Add</button>
-        </>
-    );
+      <AddDrinkFormBtn type="button" onClick={handleSubmit}>
+        Add
+      </AddDrinkFormBtn>
+    </>
+  );
 };
 
 export default AddDrinkForm;
---
+++ src/components/FollowUs/FollowUs.styled.jsx
@@ -0,0 +1,34 @@
+//   @media screen and (min-width: 768px) {
+//   }
+
+//   @media screen and (min-width: 1440px) {
+//   }
+
+import styled from 'styled-components';
+
+export const FollowUsTitle = styled.h3`
+  margin-bottom: 20px;
+
+  color: #f3f3f3;
+  color: ${({ theme }) => theme.mainText};
+  font-family: Manrope;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 1.3;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 24px;
+
+    font-size: 24px;
+    line-height: 1.3;
+  }
+`;
+
+export const FollowUsWrap = styled.div`
+  margin-bottom: 40px;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 80px;
+  }
+`;
