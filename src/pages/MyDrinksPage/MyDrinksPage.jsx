ca9d51d1f6b0afc211b22afe28e41b6d93200c6d
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-09-30 13:03:21 +0200

    Update MyDrinksPage.jsx

--- src/pages/MyDrinksPage/MyDrinksPage.jsx
+++ src/pages/MyDrinksPage/MyDrinksPage.jsx
@@ -11,17 +11,18 @@ import { Container } from '../../components/Container/Container.styled';
 
 export default function MyDrinksPage() {
   const dispatch = useDispatch();
-  const {
-    total,
-    drinks,
-  } = useDrink();
-  console.log(total);
+  const { total, drinks } = useDrink();
   const [errorMessage, setErrorMessage] = useState('');
   const [isLoading, setIsLoading] = useState(true);
-
   const [currentPage, setCurrentPage] = useState(1);
   const pageNumbersToShow = 5;
   const itemsPerPage = 6;
+
+  const onPageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const totalPages = Math.ceil(total / itemsPerPage);
 
   useEffect(() => {
     setIsLoading(true);
@@ -34,12 +35,6 @@ export default function MyDrinksPage() {
       });
   }, [currentPage, dispatch, itemsPerPage]);
 
-  const onPageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  const totalPages = Math.ceil(total / itemsPerPage);
-
   if (isLoading) {
     return <Loader />;
   }
@@ -49,28 +44,21 @@ export default function MyDrinksPage() {
       <Container>
         <PageTitle title={'My Drinks'} />
         {total === 0 && (
-          <p>
-          You have no own drinks, try to set more own drinks...
-          </p>)}
+          <p>You have no own drinks, try to set more own drinks...</p>
+        )}
         {drinks.length > 0 && (
           <ListMyDrinks>
-            <DrinksList
-            drinks={drinks} />
+            <DrinksList drinks={drinks} />
           </ListMyDrinks>
         )}
-
-        {errorMessage &&
-          <p>
-            {errorMessage}
-          </p>
-        }
-        
+        {errorMessage && <p>{errorMessage}</p>}
         {totalPages > 1 && (
           <Paginator
             drinksPerPage={itemsPerPage}
             totalDrinks={total}
             onPageChange={onPageChange}
-            pageNumbersToShow={pageNumbersToShow} />
+            pageNumbersToShow={pageNumbersToShow}
+          />
         )}
       </Container>
     </MyDrinksContainer>
